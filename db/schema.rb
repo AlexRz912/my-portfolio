3d108c90d0ba0e10b2355b3f65e8dc81b296ac98
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-04-14 16:38:02 +0200

    created model and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_14_143503) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "name"
+    t.text "imageurl"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
