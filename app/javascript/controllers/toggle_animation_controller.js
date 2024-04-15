0dee72feaa317c73e12def179e90f8bb87f4cf16
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-04-15 13:16:01 +0200

    sending transition end to params to use in pages controller

--- app/javascript/controllers/toggle_animation_controller.js
+++ app/javascript/controllers/toggle_animation_controller.js
@@ -3,9 +3,24 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle-animation"
 export default class extends Controller {
   static targets = [ "buttonText", "homepageText", "backgroundEaseInRight" ];
+
   addKeyframes() {
     this.homepageTextTarget.classList.add("homepage-texts-fade-out-left");
     this.buttonTextTarget.classList.add("button-fade-out-down", "disable-homepage-button");
     this.backgroundEaseInRightTarget.classList.add("background-fade-in-right", "background-opacity");
+    this.transitionEndHandle();
+  }
+
+  transitionEndHandle() {
+    this.backgroundEaseInRightTarget.addEventListener("animationend", () => {
+      fetch('/controller/action', {
+        method: 'POST',
+        headers: { //Comprendre ce qu'il se passe dans headers, reviser les cours sur l'ajax
+          'Content-Type': 'application/json',
+          'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+        },
+        body: JSON.stringify({ transition_finished: "true" })
+      });
+    });
   }
 }
