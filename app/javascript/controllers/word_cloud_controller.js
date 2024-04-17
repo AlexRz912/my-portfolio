c5a505646191ce4ae38da31e1ab8610397239231
Alexandre Rodriguez <alexandre.rodriguez.arz@gmail.com>
2024-04-17 02:29:59 +0200

    added randomness to sizes of project names

--- app/javascript/controllers/word_cloud_controller.js
+++ app/javascript/controllers/word_cloud_controller.js
@@ -11,18 +11,42 @@ export default class extends Controller {
 
 
   connect() {
-    
 
-    this.textsTargets.forEach((textGroup) => {
-      textGroup.style.opacity = "50%";
-      Array.from(textGroup.children).forEach((text) => {
-        text.classList.add(colorClasses[Math.floor(Math.random() * colorClasses.length)]); // Ajoutez votre classe à child
-      });
-    });
-
-
+    this.handleProjects(this.textsTargets);
     console.log(this.textsTargets);
   }
 
+  handleProjects(target) {
+    target.forEach((textGroup) => {
+      this.setColors(textGroup); //set colors to texts for each group
+      this.setSizes(textGroup);
+      this.sortByDimensions(textGroup);
+    });
+  }
+
+  setColors(textGroup) {
+    textGroup.style.opacity = "60%";
+    let colorClasses = ["hello-title-color", "pres-subtitle-color", "here-subtitle-color", "portfolio-title-color"];
+    Array.from(textGroup.children).forEach((text) => {
+      let colorClass = colorClasses[Math.floor(Math.random() * colorClasses.length)];
+      text.classList.add(colorClass); // Ajoutez votre classe à child
+
+    });
+  }
+
+  setSizes(textGroup) {
+    let fontSizeMultiples = [0.5, 1, 1.5, 2];
+    Array.from(textGroup.children).forEach((text) => {
+      let fontSizeMultiplier = fontSizeMultiples[Math.floor(Math.random() * fontSizeMultiples.length)];
+      text.style.fontSize = `${40 * fontSizeMultiplier}px`; // Ajoutez votre classe à child
+      fontSizeMultiples.splice(fontSizeMultiples.indexOf(fontSizeMultiplier), 1);
+    });
+  }
+
+  sortByDimensions(textGroup) {
+    Array.from(textGroup.children).forEach((text) => {
+      text.setAttribute('data-width', text.getBoundingClientRect().width);
+    });
+  }
 
 }
